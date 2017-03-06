7562a9308c4a793618606e45104a53eed8d6dde1
adammertel <mertel.adam@gmail.com>
2017-03-06 17:21:24 +0100

    styling, text

--- src/regulargridcluster.component.text.js
+++ src/regulargridcluster.component.text.js
@@ -1,24 +1,21 @@
 L.RegularGridClusterText = L.Marker.extend({
   options: {
-    style: {border: '0px !important'},
   },
 
   initialize: function (centroid, options) {
     this.options = L.extend(this.options, options);
     L.Util.setOptions(this, options);
 
-    // console.log(this.options);
-
     var iconOptions = JSON.stringify(options).substring(1, JSON.stringify(options).length - 2).replace(/,/g, ';').replace(/\"/g, "");
     //console.log(iconOptions);
 
     options.icon = L.divIcon({
-      html: '<span style="' + iconOptions + ' ; text-align: center">' + this.options.text + '</span>',
+      html: '<span class="regular-grid-text-html" style="' + iconOptions + ' ; text-align: center">' + this.options.text + '</span>',
       iconSize: [0, 0],
-      iconAnchor: [options.anchorOffsetX || -10, options.anchorOffsetY || - 30]
+      iconAnchor: [options.anchorOffsetX || -10, options.anchorOffsetY || - 30],
+      className: 'regular-grid-text-marker'
     });
 
-    options.border = '3px solid black';
     L.Marker.prototype.initialize.call(this, centroid, options);
   },
 
