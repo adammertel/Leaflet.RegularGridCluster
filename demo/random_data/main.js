4587a1d2819752f81cc3c3d9787eba885c26e4ad
adammertel <mertel.adam@gmail.com>
2017-11-27 10:58:45 +0100

    demo refactor

--- demo/random_data/main.js
+++ demo/random_data/main.js
@@ -8,6 +8,11 @@ var randomData = [];
 
 var grid;
 
+var elementValue = (id, parse = false) => {
+  const value = document.getElementById(id).value;
+  return parse ? parseInt(value) : value;
+};
+
 document.addEventListener('DOMContentLoaded', function(event) {
   console.log('dom loaded');
 
@@ -49,17 +54,14 @@ var render = () => {
   // define RegularGridCluster instance
   grid = L.regularGridCluster({
     rules: getRules(),
-    zoomShowElements: parseInt(
-      document.getElementById('select-elements-zoom').value
-    ),
-    zoomHideGrid: parseInt(document.getElementById('select-grid-zoom').value),
-    zoneSize: parseInt(document.getElementById('select-zone-size').value),
-    gridMode: document.getElementById('select-grid-mode').value,
-    showCells: document.getElementById('select-show-cells').value == '1',
-    showEmptyCells:
-      document.getElementById('select-show-empty-cells').value == '1',
-    showMarkers: document.getElementById('select-show-markers').value == '1',
-    showTexts: document.getElementById('select-show-texts').value == '1',
+    zoomShowElements: elementValue('select-elements-zoom', true),
+    zoomHideGrid: elementValue('select-grid-zoom', true),
+    zoneSize: elementValue('select-zone-size', true),
+    gridMode: elementValue('select-grid-mode'),
+    showCells: elementValue('select-show-cells') === '1',
+    showEmptyCells: elementValue('select-show-empty-cells') === '1',
+    showMarkers: elementValue('select-show-markers') === '1',
+    showTexts: elementValue('select-show-texts') === '1',
     trackingTime: true
   });
 
