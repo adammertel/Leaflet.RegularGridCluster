cb4b24d1b02678cdfe317f13f5ee9394eb50108a
adammertel <mertel.adam@gmail.com>
2017-11-27 10:38:15 +0100

    leaflet dependency loading from bower folder

--- demo/random_data/main.js
+++ demo/random_data/main.js
@@ -1,40 +1,47 @@
 var map;
-var maxX = 50, minX = 0, maxY = 49.5, minY = 0;
+var maxX = 50,
+  minX = 0,
+  maxY = 49.5,
+  minY = 0;
 var noTestData = 1000;
 var randomData = [];
 
 var grid;
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  console.log('dom loaded')
+document.addEventListener('DOMContentLoaded', function(event) {
+  console.log('dom loaded');
 
-  const colors = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00'];
-  createRandomData()
+  const colors = ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00'];
+  createRandomData();
 
   // setting map
   map = L.map('map-content');
   map.fitBounds([[minY, minX], [maxY, maxX]]);
 
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-    opacity: .3
+    attribution:
+      '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    opacity: 0.3
   }).addTo(map);
 
-  var selects = [].slice.call(document.getElementsByClassName('render-on-change'), 0);
+  var selects = [].slice.call(
+    document.getElementsByClassName('render-on-change'),
+    0
+  );
   for (var si in selects) {
     var select = selects[si];
 
     if (select.addEventListener) {
-      select.addEventListener('change', function () {
-        render()
+      select.addEventListener('change', function() {
+        render();
       });
-    };
-  }; 
+    }
+  }
 
   render();
-})
+});
 
-var render = function () {
+var render = function() {
   console.log('demo renders');
   if (map.hasLayer(grid)) {
     grid.unregister();
@@ -42,28 +49,29 @@ var render = function () {
   }
 
   // define RegularGridCluster instance
-  var grid = L.regularGridCluster(
-    {
-      rules: getRules(),
-      zoomShowElements: parseInt(document.getElementById('select-elements-zoom').value),
-      zoomHideGrid: parseInt(document.getElementById('select-grid-zoom').value),
-      zoneSize: parseInt(document.getElementById('select-zone-size').value),
-      gridMode: document.getElementById('select-grid-mode').value,
-      showCells: (document.getElementById('select-show-cells').value == "1"),
-      showEmptyCells: (document.getElementById('select-show-empty-cells').value == "1"),
-      showMarkers: (document.getElementById('select-show-markers').value == "1"),
-      showTexts: (document.getElementById('select-show-texts').value == "1"),
-      trackingTime: true
-    }
-  );
+  var grid = L.regularGridCluster({
+    rules: getRules(),
+    zoomShowElements: parseInt(
+      document.getElementById('select-elements-zoom').value
+    ),
+    zoomHideGrid: parseInt(document.getElementById('select-grid-zoom').value),
+    zoneSize: parseInt(document.getElementById('select-zone-size').value),
+    gridMode: document.getElementById('select-grid-mode').value,
+    showCells: document.getElementById('select-show-cells').value == '1',
+    showEmptyCells:
+      document.getElementById('select-show-empty-cells').value == '1',
+    showMarkers: document.getElementById('select-show-markers').value == '1',
+    showTexts: document.getElementById('select-show-texts').value == '1',
+    trackingTime: true
+  });
 
   grid.addLayers(randomData);
   grid.addTo(map);
-}
+};
 
-const createRandomData = function () {
+const createRandomData = function() {
   // putting some random point data
-  for (var i=0; i < noTestData; i++) {
+  for (var i = 0; i < noTestData; i++) {
     const coordinates = [
       minX + Math.random() * (maxX - minX),
       minY + Math.random() * (maxY - minY)
@@ -74,12 +82,11 @@ const createRandomData = function () {
     };
 
     const marker = L.circleMarker(coordinates, circleStyle(properties));
-    randomData.push({marker: marker, properties: properties});
-  };
-}
+    randomData.push({ marker: marker, properties: properties });
+  }
+};
 
-
-const parseTextAreaValue = function (textAreaId) {
+const parseTextAreaValue = function(textAreaId) {
   const textAreaValue = document.getElementById(textAreaId).value;
   const textAreaObjectValue = '{' + textAreaValue + '}';
 
@@ -87,29 +94,29 @@ const parseTextAreaValue = function (textAreaId) {
     return JSON.parse(textAreaObjectValue);
   } catch (err) {
     console.log(err);
-    alert('bad input ' + textAreaId + ', ' +  err);
-    return {}
+    alert('bad input ' + textAreaId + ', ' + err);
+    return {};
   }
-}
+};
 
-const getRules = function () {
+const getRules = function() {
   const rulesTextCells = parseTextAreaValue('textarea-rules-cells');
   const rulesTextMarkers = parseTextAreaValue('textarea-rules-markers');
   const rulesTextTexts = parseTextAreaValue('textarea-rules-texts');
 
   return {
-    "cells": rulesTextCells,
-    "markers": rulesTextMarkers,
-    "texts": rulesTextTexts
-  }
-} 
+    cells: rulesTextCells,
+    markers: rulesTextMarkers,
+    texts: rulesTextTexts
+  };
+};
 
-const circleStyle = function (props) {
+const circleStyle = function(props) {
   return {
-    fillColor: ['#ffffcc','#a1dab4','#41b6c4','#2c7fb8','#253494'][props.b],
+    fillColor: ['#ffffcc', '#a1dab4', '#41b6c4', '#2c7fb8', '#253494'][props.b],
     color: 'black',
     weight: 1,
     radius: props.a / 3,
     fillOpacity: 1
-  }
-}
+  };
+};
